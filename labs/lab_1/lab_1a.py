6be64dd323e5c618a0d0c52824796552133fc70d
Ruta S <rutasirdeshmukh@gmail.com>
2026-02-14 16:20:23 -0800

    Add intro to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Ruta" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("My name is " + name + " and I am excited to learn CS!")
 
 if __name__ == "__main__":
     main()
